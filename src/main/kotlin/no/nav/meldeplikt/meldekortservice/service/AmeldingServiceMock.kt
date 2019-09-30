bd922073e711cc6ac43b6fe22bbd7996615df66c
Yrjan Fraschetti <yrjanaff@gmail.com>
2019-09-30 11:20:18 +0200

    Forenkler mock av kontrollerMeldekort

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/AmeldingServiceMock.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/AmeldingServiceMock.kt
@@ -1,26 +1,12 @@
 package no.nav.meldeplikt.meldekortservice.service
 
-import no.aetat.amelding.externcontrolemelding.webservices.ExternControlEmeldingSOAP
-import no.aetat.arena.mk_meldekort_kontrollert.ArsakskodeType
 import no.aetat.arena.mk_meldekort.MeldekortType
-import no.aetat.arena.mk_meldekort_kontrollert.MeldekortDagType
 import no.aetat.arena.mk_meldekort_kontrollert.MeldekortKontrollertType
 
 class AmeldingServiceMock(): AmeldingService {
 
     override fun kontrollerMeldekort(meldekort: MeldekortType): MeldekortKontrollertType {
-        val arsakskodeType = ArsakskodeType()
-        arsakskodeType.kode = "kode"
-        arsakskodeType.tekst = "tekst"
-
-        val meldekortDagType = MeldekortDagType()
-        meldekortDagType.dag = 1
-        meldekortDagType.meldegruppe = "DAGP"
-
-
         val meldekortKontrollertType = MeldekortKontrollertType()
-        meldekortKontrollertType.arsakskoder.arsakskode.add(arsakskodeType)
-        meldekortKontrollertType.meldekortDager.meldekortDag.add(meldekortDagType)
         meldekortKontrollertType.meldekortId = meldekort.hode.meldekortId.verdi
         meldekortKontrollertType.status = "OK"
 
