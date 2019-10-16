5842e74ed4e9c6d482d91fe40e4ef44c60dbb08c
Yrjan Fraschetti <yrjanaff@gmail.com>
2019-10-16 08:57:01 +0200

    Legger til grant_type som query param og client id og secret som base64 encodet header

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/ArenaOrdsService.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/ArenaOrdsService.kt
@@ -4,6 +4,8 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.post
+import io.ktor.util.InternalAPI
+import io.ktor.util.encodeBase64
 import kotlinx.coroutines.runBlocking
 import no.nav.meldeplikt.meldekortservice.config.Environment
 import no.nav.meldeplikt.meldekortservice.config.cache
@@ -11,6 +13,7 @@ import no.nav.meldeplikt.meldekortservice.config.client
 import no.nav.meldeplikt.meldekortservice.model.OrdsToken
 import no.nav.meldeplikt.meldekortservice.utils.ARENA_ORDS_TOKEN_PATH
 import no.nav.meldeplikt.meldekortservice.utils.isCurrentlyRunningOnNais
+import java.util.*
 
 object ArenaOrdsService {
 
@@ -28,7 +31,7 @@ object ArenaOrdsService {
 
     private fun hentOrdsToken(): OrdsToken {
         println("Cache timet ut. Henter token")
-        var token = OrdsToken("", "", 0)
+        var token = OrdsToken(null, null, null)
 
         if (isCurrentlyRunningOnNais()) {
             runBlocking {
@@ -46,12 +49,11 @@ object ArenaOrdsService {
 
     private fun HttpRequestBuilder.setupOrdsRequest() {
         headers.append("Accept", "application/xml")
-        headers.append("Authorization","Bearer ${hentToken()}")
+        headers.append("Authorization","Bearer ${hentToken()}?grant_type=client_credentials")
     }
 
     private fun HttpRequestBuilder.setupTokenRequest() {
-        headers.append("client_id", env.ordsClientId)
-        headers.append("client_scret", env.ordsClientSecret)
-        headers.append("grant_type", "client_credentials")
+        val base = "${env.ordsClientId}:${env.ordsClientSecret}"
+        headers.append("Authorization", "Basic ${Base64.getEncoder().encode(base.toByteArray())}")
     }
 }
