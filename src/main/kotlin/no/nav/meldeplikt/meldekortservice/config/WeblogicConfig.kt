431218274dfbb73a7d370a91f4a7876b3ac38ffa
Yrjan Fraschetti <yrjanaff@gmail.com>
2019-10-25 12:45:10 +0200

    Legger til SchemaValidation

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/config/WeblogicConfig.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/config/WeblogicConfig.kt
@@ -2,17 +2,17 @@ package no.nav.meldeplikt.meldekortservice.config
 
 import no.nav.sbl.dialogarena.common.cxf.CXFClient
 import no.nav.tjeneste.virksomhet.sakogaktivitet.v1.SakOgAktivitetV1
+import org.apache.cxf.annotations.SchemaValidation
 
 object WeblogicConfig {
 
     private val environment = Environment()
 
+    @SchemaValidation
     fun arbeidOgAktivitetSakV1(): SakOgAktivitetV1{
         return CXFClient(SakOgAktivitetV1::class.java)
             .address(environment.sakOgAktivitetUri)
             .configureStsForSystemUser()
             .build()
     }
-
-
 }
