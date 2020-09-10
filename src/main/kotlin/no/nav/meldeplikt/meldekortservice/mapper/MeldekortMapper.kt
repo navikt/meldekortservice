27add0062f3463a33a13041887ea9552769d0eb7
audun-persson <audun.persson@nav.no>
2020-09-10 14:18:12 +0200

    Rullet tilbake test

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/mapper/MeldekortMapper.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/mapper/MeldekortMapper.kt
@@ -27,18 +27,12 @@ object MeldekortMapper {
 
     private fun erMeldekortSendtInnTidligere(meldekortId: Long, meldekortService: InnsendtMeldekortService): Boolean {
         return try {
-            // test
-            throw SQLException("Klarer ikke å lese innsendte meldekort fra MIP-tabellen")
             meldekortService.hentInnsendtMeldekort(meldekortId)
             true
         } catch (se: SQLException) {
             if (se.message == "Found no rows") {
                 false
             } else {
-                // Test
-                val errorMessage =
-                    ErrorMessage("Feil ved lesing fra MIP-tabellen. Klarer ikke å sjekke om meldekort med id ${meldekortId} er sendt inn tidligere. ${se.message}")
-                defaultLog.warn(errorMessage.error, se)
                 throw UnretriableDatabaseException("Feil ved lesing av innsendte meldekort", se)
             }
         }
