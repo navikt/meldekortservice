6833eb178c740e811797241b0341ee185fc91c02
audun-persson <audun.persson@nav.no>
2020-09-10 17:55:18 +0200

    Tar fiks av lesing i egen branch

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/mapper/MeldekortMapper.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/mapper/MeldekortMapper.kt
@@ -4,8 +4,6 @@ import no.nav.meldeplikt.meldekortservice.model.database.feil.UnretriableDatabas
 import no.nav.meldeplikt.meldekortservice.model.meldekort.Meldekort
 import no.nav.meldeplikt.meldekortservice.model.meldekort.Person
 import no.nav.meldeplikt.meldekortservice.service.InnsendtMeldekortService
-import no.nav.meldeplikt.meldekortservice.utils.ErrorMessage
-import no.nav.meldeplikt.meldekortservice.utils.defaultLog
 import java.sql.SQLException
 
 object MeldekortMapper {
@@ -30,17 +28,12 @@ object MeldekortMapper {
 
     private fun erMeldekortSendtInnTidligere(meldekortId: Long, meldekortService: InnsendtMeldekortService): Boolean {
         return try {
-            throw SQLException("test")
             meldekortService.hentInnsendtMeldekort(meldekortId)
             true
         } catch (se: SQLException) {
             if (se.message == "Found no rows") {
                 false
             } else {
-                val errorMessage =
-                    ErrorMessage("Forsøkte å sjekke om meldekort med id ${meldekortId} er sendt inn tidligere, men klarte ikke å lese fra MIP-tabellen. ${se.message}")
-                defaultLog.warn(errorMessage.error, se)
-                //false
                 throw UnretriableDatabaseException("Feil ved lesing av innsendte meldekort", se)
             }
         }
