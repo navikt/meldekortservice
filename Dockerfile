88d997f2e507564cab5ac4c585178fa23571afea
audun-persson <audun.persson@nav.no>
2020-10-20 16:55:34 +0200

    Endret lesing av secrets

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM navikt/java:11
+COPY export-vault-creds.sh /init-scripts/03-export-vault-creds.sh
 COPY build/libs/meldekortservice.jar /app/app.jar
 EXPOSE 8090
--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/config/Environment.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/config/Environment.kt
@@ -1,21 +1,10 @@
 package no.nav.meldeplikt.meldekortservice.config
 
-import com.bettercloud.vault.SslConfig
-import com.bettercloud.vault.Vault
-import com.bettercloud.vault.VaultConfig
-import com.bettercloud.vault.VaultException
-import com.bettercloud.vault.json.Json
-import no.nav.meldeplikt.meldekortservice.utils.defaultLog
-import no.nav.meldeplikt.meldekortservice.utils.isCurrentlyRunningOnNais
-import no.nav.meldeplikt.meldekortservice.utils.vaultTokenPath
-import no.nav.meldeplikt.meldekortservice.utils.vaultUrl
 import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Paths
 
 data class Environment(
-    val username: String = getEnvVar("FSS_SYSTEMUSER_USERNAME", "username"),
-    val password: String = getEnvVar("FSS_SYSTEMUSER_PASSWORD", "password"),
+//    val username: String = getEnvVar("FSS_SYSTEMUSER_USERNAME", "username"),
+//    val password: String = getEnvVar("FSS_SYSTEMUSER_PASSWORD", "password"),
     val ameldingUrl: URL = URL(getEnvVar("AMELDING_URI", "https://dummyUrl.com/path")),
     val ordsUrl: URL = URL(getEnvVar("ORDS_URI", "https://dummyUrl.com")),
     val ordsClientId: String = getEnvVar("CLIENT_ID", "cLiEnTiD"),
@@ -31,12 +20,21 @@ data class Environment(
     val dbPasswordPostgreSQL: String = getEnvVar("DB_PASSWORD", "testpassword"),
 
     // Oracle
-    val dbUserOracleKvPath: String = getEnvVar("MELDEKORTSERVICE_DS_CREDS_KV_PATH", "path"),
-    val dbConfOracleKvPath: String = getEnvVar("MELDEKORTSERVICE_DS_CONF_KV_PATH", "path"),
+    val dbUserOracle: VaultCredentials = VaultCredentials(
+        getEnvVar("DB_USER_MELDEKORTSERVICE_USERNAME"),
+        getEnvVar("DB_USER_MELDEKORTSERVICE_PASSWORD")
+    ),
+    val dbConfOracle: VaultDbConfig = VaultDbConfig(getEnvVar("DB_CONFIG_MELDEKORTSERVICE_JDBCURL")),
 
     // Serviceusers
-    val serviceUserKvPath: String = getEnvVar("SERVICE_USER_KV_PATH", "path"),
-    val srvSblArbeidPath: String = getEnvVar("SRV_SBL_ARBEID_PATH", "path")
+    val srvMeldekortservice: VaultCredentials = VaultCredentials(
+        getEnvVar("SERVICEUSER_MELDEKORTSERVICE_USERNAME"),
+        getEnvVar("SERVICEUSER_MELDEKORTSERVICE_PASSWORD")
+    ),
+    val srvSblArbeid: VaultCredentials = VaultCredentials(
+        getEnvVar("SERVICEUSER_SBLARBEID_USERNAME"),
+        getEnvVar("SERVICEUSER_SBLARBEID_PASSWORD")
+    )
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null): String {
@@ -44,54 +42,10 @@ fun getEnvVar(varName: String, defaultValue: String? = null): String {
     ?: throw IllegalArgumentException("Variabelen $varName kan ikke være tom")
 }
 
-private fun vault() = Vault(
-    VaultConfig()
-        .address(vaultUrl)
-        .token(String(Files.readAllBytes(Paths.get(vaultTokenPath))))
-        .openTimeout(5)
-        .readTimeout(30)
-        .sslConfig(SslConfig().build())
-        .build()
-)
-
-fun hentVaultCredentials(path: String): VaultCredentials {
-    return if (isCurrentlyRunningOnNais()) {
-        try {
-            val credentials = Json.parse(vault().logical().read(path).data["data"]).asObject()
-            VaultCredentials(credentials.get("username").asString(), credentials.get("password").asString())
-        } catch (e: VaultException) {
-            when (e.httpStatusCode) {
-                403 -> defaultLog.error("Vault denied permission to fetch credentials for path '$path'", e)
-                else -> defaultLog.error("Could not fetch credentials for path '$path'", e)
-            }
-            throw e
-        }
-    } else {
-        VaultCredentials("test", "test")
-    }
-}
-
 data class VaultCredentials(
     val username: String?,
     val password: String?
 )
-
-fun hentVaultDbConfig(path: String): VaultDbConfig {
-    return if (isCurrentlyRunningOnNais()) {
-        try {
-            val config = Json.parse(vault().logical().read(path).data["data"]).asObject()
-            VaultDbConfig(config.get("jdbc_url").asString())
-        } catch (e: VaultException) {
-            when (e.httpStatusCode) {
-                403 -> defaultLog.error("Vault denied permission to fetch database configuration for path '$path'", e)
-                else -> defaultLog.error("Could not fetch database configuration for path '$path'", e)
-            }
-            throw e
-        }
-    } else {
-        VaultDbConfig("test")
-    }
-}
 
 data class VaultDbConfig(
     val jdbcUrl: String?
