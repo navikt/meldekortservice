fde7e1be99a6e4a23e07f116a718adf2188606a9
s144556 <geir.skjeret@nav.no>
2021-09-14 15:54:47 +0200

    La til kontrollstatus

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/model/meldekortdetaljer/kontroll/response/KontrollResponse.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/model/meldekortdetaljer/kontroll/response/KontrollResponse.kt
@@ -3,6 +3,7 @@ package no.nav.meldeplikt.meldekortservice.model.meldekortdetaljer.kontroll.resp
 data class KontrollResponse constructor(
     var meldekortId: Long = 0,
     var status: String = "",
+    var kontrollStatus: String = "",
     var feilListe: List<KontrollFeil> = emptyList(),
     var oppfolgingListe: List<KontrollFeil> = emptyList()
 )
