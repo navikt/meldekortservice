bed76ef9b4c1842fc29c1eaa51a06770333cad01
Igor Shuliakov <igor.shuliakov@nav.no>
2021-09-27 21:52:41 +0200

    MELDEPLIKT-96 Fikset CLOB

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/database/Queries.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/database/Queries.kt
@@ -5,14 +5,15 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.meldeplikt.meldekortservice.model.database.InnsendtMeldekort
 import no.nav.meldeplikt.meldekortservice.model.dokarkiv.Journalpost
 import java.io.Reader
+import java.io.Writer
 import java.nio.ByteBuffer
 import java.nio.CharBuffer
 import java.nio.charset.StandardCharsets
+import java.sql.Clob
 import java.sql.Connection
 import java.sql.DatabaseMetaData
 import java.sql.ResultSet
 import java.util.*
-import javax.sql.rowset.serial.SerialClob
 
 
 fun Connection.hentInnsendtMeldekort(meldekortId: Long): InnsendtMeldekort =
@@ -43,8 +44,15 @@ fun Connection.lagreJournalpostData(journalpostId: Long, dokumentInfoId: Long, m
 fun Connection.lagreJournalpostMidlertidig(journalpost: Journalpost): Int =
     prepareStatement("""INSERT INTO MIDLERTIDIG_LAGREDE_JOURNALPOSTER (id, journalpost, created, retries) VALUES (?, ?, ?, ?)""")
         .use {
+            val clob: Clob = this.createClob()
+
+            val out: Writer = clob.setCharacterStream(1L)
+            out.write(bytesToChars(ObjectMapper().writeValueAsBytes(journalpost)))
+            out.flush()
+            out.close()
+
             it.setString(1, journalpost.eksternReferanseId) // Vi vet at det er UUID der
-            it.setClob(2, SerialClob(bytesToChars(ObjectMapper().writeValueAsBytes(journalpost))))
+            it.setClob(2, clob)
             it.setLong(3, System.currentTimeMillis() / 1000)
             it.setLong(4, 0)
             it.executeUpdate()
