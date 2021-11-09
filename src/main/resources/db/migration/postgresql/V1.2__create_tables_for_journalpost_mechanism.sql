a4a0fa20177fc39fb739b4956d75f1d6ac92769b
Igor Shuliakov <igor.shuliakov@nav.no>
2021-11-09 12:43:51 +0100

    MELDEPLIKT-96 Index i opprettede_journalposter

--- src/main/resources/db/migration/postgresql/V1.2__create_tables_for_journalpost_mechanism.sql
+++ src/main/resources/db/migration/postgresql/V1.2__create_tables_for_journalpost_mechanism.sql
@@ -6,6 +6,8 @@ CREATE TABLE opprettede_journalposter
     created        char(19) default to_char(current_timestamp, 'YYYY-MM-DD HH24:MI:SS') not null
 );
 
+CREATE INDEX opprettede_journalposter_meldekortid_index ON opprettede_journalposter (meldekortid);
+
 CREATE TABLE midlertidig_lagrede_journalposter
 (
     id          char(36) primary key,
