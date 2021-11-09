5e4f758ea7a5c613dbde8b1a1b6c7eba541d0f54
Igor Shuliakov <igor.shuliakov@nav.no>
2021-11-09 11:34:18 +0100

    MELDEPLIKT-96 Default for Created

--- src/main/resources/db/migration/oracle/V1.2__create_tables_for_journalpost_mechanism.sql
+++ src/main/resources/db/migration/oracle/V1.2__create_tables_for_journalpost_mechanism.sql
@@ -1,15 +1,15 @@
 CREATE TABLE OPPRETTEDE_JOURNALPOSTER
 (
-    journalpostId  numeric  primary key,
-    dokumentInfoId numeric  not null,
-    meldekortId    numeric  not null,
-    created        char(19) not null
+    journalpostId  numeric primary key,
+    dokumentInfoId numeric                                                              not null,
+    meldekortId    numeric                                                              not null,
+    created        char(19) default to_char(current_timestamp, 'YYYY-MM-DD HH24:MI:SS') not null
 );
 
 CREATE TABLE MIDLERTIDIG_LAGREDE_JOURNALPOSTER
 (
     id          char(36) primary key,
-    journalpost clob              not null,
-    created     char(19)          not null,
-    retries     numeric default 0 not null
+    journalpost clob                                                                 not null,
+    created     char(19) default to_char(current_timestamp, 'YYYY-MM-DD HH24:MI:SS') not null,
+    retries     numeric  default 0                                                   not null
 );
