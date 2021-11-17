70ec338a29e865c733ccd4ef4634e54c9d7ef24c
s144556 <geir.skjeret@nav.no>
2021-11-17 09:13:42 +0100

    logging for debug

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/SoapServiceImpl.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/SoapServiceImpl.kt
@@ -2,10 +2,13 @@ package no.nav.meldeplikt.meldekortservice.service
 
 import no.aetat.amelding.externcontrolemelding.webservices.ExternControlEmeldingSOAP
 import no.aetat.arena.mk_meldekort_kontrollert.MeldekortKontrollertType
+import no.nav.meldeplikt.meldekortservice.api.jsonMapper
+import no.nav.meldeplikt.meldekortservice.api.maskerFnrIAmeldingMeldekort
 import no.nav.meldeplikt.meldekortservice.config.SoapConfig
 import no.nav.meldeplikt.meldekortservice.mapper.MeldekortTypeMapper
 import no.nav.meldeplikt.meldekortservice.model.WeblogicPing
 import no.nav.meldeplikt.meldekortservice.model.meldekortdetaljer.Meldekortdetaljer
+import no.nav.meldeplikt.meldekortservice.utils.defaultLog
 import no.nav.meldeplikt.meldekortservice.utils.getLogger
 import no.nav.tjeneste.virksomhet.sakogaktivitet.v1.SakOgAktivitetV1
 
@@ -19,6 +22,11 @@ class SoapServiceImpl(
 
     override fun kontrollerMeldekort(meldekortdetaljer: Meldekortdetaljer): MeldekortKontrollertType {
         val meldekort = MeldekortTypeMapper.mapMeldekortType(meldekortdetaljer)
+        defaultLog.info(
+            "Mappet meldekort er: " + jsonMapper.writeValueAsString(
+                meldekort)
+            )
+        
         return amelding.kontrollerEmeldingMeldekort(meldekort)
     }
 
