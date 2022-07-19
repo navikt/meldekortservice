1d3cc1e34daa35e9f8d014a92eca8ee22d51c746
Igor Shuliakov <igor.shuliakov@nav.no>
2022-07-19 08:54:12 +0200

    MELDEPLIKT-425 Nye versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,13 @@ application {
 dependencies {
 
     implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-bindings-soap:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
+    implementation("org.apache.cxf:cxf-core:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-frontend-simple:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
+    implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
 
     implementation(kotlin("stdlib"))
     implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
