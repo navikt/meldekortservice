8537740f1daeeebfeb8aa8706f6d10eeec4b1a62
Igor Shuliakov <igor.shuliakov@nav.no>
2022-07-20 12:44:01 +0200

    MELDEPLIKT-425 Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     implementation("io.ktor:ktor-server-locations:$ktorVersion")
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.micrometer:micrometer-registry-prometheus:$ktorVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
