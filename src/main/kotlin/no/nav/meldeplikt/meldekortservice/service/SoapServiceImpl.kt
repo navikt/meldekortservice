ef1637336700b3b13a2c98ebb1986d911cb0634b
Igor Shuliakov <igor.shuliakov@nav.no>
2022-07-20 20:21:56 +0200

    Soap log

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/SoapServiceImpl.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/service/SoapServiceImpl.kt
@@ -1,26 +1,30 @@
 package no.nav.meldeplikt.meldekortservice.service
 
+import mu.KLogging
 import no.nav.common.cxf.StsConfig
 import no.nav.meldeplikt.meldekortservice.config.SoapConfig
 import no.nav.meldeplikt.meldekortservice.model.WeblogicPing
 import no.nav.meldeplikt.meldekortservice.utils.*
 import no.nav.tjeneste.virksomhet.sakogaktivitet.v1.SakOgAktivitetV1
+import java.lang.System.getProperty
 
 class SoapServiceImpl(
     private val oppfoelgingPing: SakOgAktivitetV1? = SoapConfig.sakOgAktivitet().configureStsForSystemUser(
         StsConfig.builder()
-            .url(System.getProperty(STS_URL_KEY) + STS_PATH)
-            .username(System.getProperty(SYSTEMUSER_USERNAME))
-            .password(System.getProperty(SYSTEMUSER_PASSWORD)).build()
+            .url(getProperty(STS_URL_KEY) + STS_PATH)
+            .username(getProperty(SYSTEMUSER_USERNAME))
+            .password(getProperty(SYSTEMUSER_PASSWORD)).build()
     ).build()
 ) : SoapService {
+
+    companion object : KLogging()
 
     override fun pingWeblogic(): WeblogicPing {
         return try {
             oppfoelgingPing?.ping()
             WeblogicPing(true)
         } catch (e: Exception) {
-            defaultLog.info("Ingen svar fra WebLogic, ping feilet", e)
+            logger.info("Ingen svar fra WebLogic, ping feilet", e)
             WeblogicPing(false)
         }
     }
