57995fbc0e2050fe31e436de3a5fb1df0eaae515
Igor Shuliakov <igor.shuliakov@nav.no>
2022-10-10 15:18:36 +0200

    MELDEPLIKT-345 Send journalpost på nytt interval 5 min

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/config/Environment.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/config/Environment.kt
@@ -60,7 +60,7 @@ data class Environment(
     val stsNaisUrl: String = removeTrailingSlash(getEnvVar("SECURITYTOKENSERVICE_NAIS", "https://ststest.nav.no/")),
 
     val dokarkivUrl: String = removeTrailingSlash(getEnvVar("DOKARKIV_URL", "https://dokarkivtest.nav.no/")),
-    val dokarkivResendInterval: Long = getEnvVar("DOKARKIV_RESEND_INTERVAL", "30000").toLong()
+    val dokarkivResendInterval: Long = getEnvVar("DOKARKIV_RESEND_INTERVAL", "300000").toLong() // 5 minutes by default
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null): String {
