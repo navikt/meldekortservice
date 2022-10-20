c30807e3b6ecce09af0d7752fb4a053f4033f7f7
Igor Shuliakov <igor.shuliakov@nav.no>
2022-10-20 14:22:02 +0200

    MELDEPLIKT-345 Fikset logikken

--- src/test/kotlin/no/nav/meldeplikt/meldekortservice/service/KontrollServiceTest.kt
+++ src/test/kotlin/no/nav/meldeplikt/meldekortservice/service/KontrollServiceTest.kt
@@ -14,11 +14,39 @@ import no.nav.meldeplikt.meldekortservice.model.meldekortdetaljer.kontroll.respo
 import no.nav.meldeplikt.meldekortservice.model.meldekortdetaljer.kontroll.response.KontrollResponse
 import no.nav.meldeplikt.meldekortservice.utils.defaultObjectMapper
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import kotlin.test.assertEquals
 
 class KontrollServiceTest {
+
+    val meldekortkontroll = Meldekortkontroll(
+        meldekortId = 123,
+        fnr = "11111111111",
+        personId = 335,
+        kilde = "MELDEPLIKT",
+        kortType = "ELEKTRONISK",
+        periodeFra = LocalDate.parse("2020-01-20", DateTimeFormatter.ISO_DATE),
+        periodeTil = LocalDate.parse("2020-02-02", DateTimeFormatter.ISO_DATE),
+        meldedato = LocalDate.parse("2020-02-03", DateTimeFormatter.ISO_DATE),
+        meldegruppe = "DAGP",
+        arbeidssoker = true,
+        arbeidet = false,
+        syk = false,
+        annetFravaer = true,
+        kurs = false,
+        begrunnelse = "Begrunnelsen er fin",
+        meldekortdager = listOf(
+            FravaerInn(
+                dato = LocalDate.parse("2020-01-21", DateTimeFormatter.ISO_DATE),
+                syk = false,
+                kurs = false,
+                annetFravaer = true,
+                arbeidTimer = 0.0
+            )
+        )
+    )
 
     @Test
     fun kontrollerSkalReturnereOk() {
@@ -38,33 +66,44 @@ class KontrollServiceTest {
         test(kontrollResponse, "FEIL")
     }
 
-    private fun test(kontrollResponse: KontrollResponse, forventetStatus: String) {
-        val meldekortkontroll = Meldekortkontroll(
-            meldekortId = 123,
-            fnr = "11111111111",
-            personId = 335,
-            kilde = "MELDEPLIKT",
-            kortType = "ELEKTRONISK",
-            periodeFra = LocalDate.parse("2020-01-20", DateTimeFormatter.ISO_DATE),
-            periodeTil = LocalDate.parse("2020-02-02", DateTimeFormatter.ISO_DATE),
-            meldedato = LocalDate.parse("2020-02-03", DateTimeFormatter.ISO_DATE),
-            meldegruppe = "DAGP",
-            arbeidssoker = true,
-            arbeidet = false,
-            syk = false,
-            annetFravaer = true,
-            kurs = false,
-            begrunnelse = "Begrunnelsen er fin",
-            meldekortdager = listOf(
-                FravaerInn(
-                    dato = LocalDate.parse("2020-01-21", DateTimeFormatter.ISO_DATE),
-                    syk = false,
-                    kurs = false,
-                    annetFravaer = true,
-                    arbeidTimer = 0.0
+    @Test
+    fun kontrollerSkalKasteException() {
+        val client = HttpClient(MockEngine) {
+            install(ContentNegotiation) {
+                register(
+                    ContentType.Application.Json,
+                    JacksonConverter(defaultObjectMapper)
                 )
-            )
+            }
+
+            engine {
+                addHandler { request ->
+                    respond(
+                        "",
+                        HttpStatusCode.InternalServerError,
+                        headersOf(HttpHeaders.ContentType, "application/json")
+                    )
+                }
+            }
+        }
+
+        val aadService: AadService = mockk()
+        coEvery { aadService.fetchAadToken() } returns "token"
+
+        val kontrollService = KontrollService(
+            aadService = aadService,
+            kontrollClient = client
         )
+
+        val exception = assertThrows<Exception> {
+            runBlocking {
+                kontrollService.kontroller(meldekortkontroll)
+            }
+        }
+        assertEquals("Meldekortkontroll returnerte status 500", exception.localizedMessage)
+    }
+
+    private fun test(kontrollResponse: KontrollResponse, forventetStatus: String) {
 
         val client = HttpClient(MockEngine) {
             install(ContentNegotiation) {
