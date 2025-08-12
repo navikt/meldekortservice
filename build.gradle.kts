6743a7a5b58c359d3b559b8a3c1adc4fbafae9dd
Igor Weber <igor.shuliakov@nav.no>
2025-08-12 10:48:48 +0200

    Shadow 9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ plugins {
     kotlin("plugin.allopen") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
 
-    id("com.gradleup.shadow") version "9.0.1"
+    id("com.gradleup.shadow") version "9.0.0"
 
     id("org.flywaydb.flyway") version ("11.11.0")
 
