434dbe3f257c33312382f2956d89450ec8670c7b
Igor Weber <85676385+igorweber@users.noreply.github.com>
2025-10-06 12:45:33 +0200

    Update Swagger UI version from 5.28.1 to 5.29.2

--- src/main/kotlin/no/nav/meldeplikt/meldekortservice/utils/swagger/SwaggerUi.kt
+++ src/main/kotlin/no/nav/meldeplikt/meldekortservice/utils/swagger/SwaggerUi.kt
@@ -19,7 +19,7 @@ class SwaggerUi {
             in notFound -> return
             null -> return
             else -> {
-                val resource = this::class.java.getResource("/META-INF/resources/webjars/swagger-ui/5.28.1/$filename")
+                val resource = this::class.java.getResource("/META-INF/resources/webjars/swagger-ui/5.29.2/$filename")
                 if (resource == null) {
                     notFound.add(filename)
                     return
